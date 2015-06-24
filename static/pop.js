2edc9eda4fc30a253f4801ef0710f30bf48de5a1
niuben <niuben1983@outlook.com>
2015-06-24 11:16:04 +0800

    laydate-seajs

--- static/pop.js
+++ static/pop.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
     var $ = require('jquery');
     var layer = require('layer');
+    
     layer.config({
         path: '../sea-modules/layer/'
             //layer.js所在的目录，可以是绝对目录，也可以是相对目录
@@ -255,5 +256,57 @@ define(function(require, exports, module) {
 
     });
 
+
+
+    //laydata
+    require.async('laydate', function() {
+        laydate({
+            elem: '#J-xl'
+        });
+
+        document.getElementById('J-xl-2-btn').onclick = function() {
+            laydate({
+                elem: '#J-xl-2'
+            });
+        }
+
+        laydate({
+            elem: '#J-xl-3'
+        });
+
+        laydate({
+            elem: document.getElementById('J-xl-4'),
+            isv: ''
+        });
+
+
+
+        var start = {
+            elem: '#start',
+            format: 'YYYY/MM/DD hh:mm:ss',
+            min: laydate.now(), //设定最小日期为当前日期
+            max: '2099-06-16 23:59:59', //最大日期
+            istime: true,
+            istoday: false,
+            choose: function(datas) {
+                end.min = datas; //开始日选好后，重置结束日的最小日期
+                end.start = datas //将结束日的初始值设定为开始日
+            }
+        };
+        var end = {
+            elem: '#end',
+            format: 'YYYY/MM/DD hh:mm:ss',
+            min: laydate.now(),
+            max: '2099-06-16 23:59:59',
+            istime: true,
+            istoday: false,
+            choose: function(datas) {
+                start.max = datas; //结束日选好后，重置开始日的最大日期
+            }
+        };
+        laydate(start);
+        laydate(end);
+
+    });
 
 });
